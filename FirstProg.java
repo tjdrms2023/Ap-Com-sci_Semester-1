635a580ca92d9bea8ce07503815b632aafe306d8
tjdrms2023 <tjdrms2023@gmail.com>
2016-09-06 08:27:38 -0700

    Lesson 01

---
+++ FirstProg.java
@@ -0,0 +1,11 @@
+public class FirstProg
+{
+	public static void main(String[] args)
+	{
+        System.out.println("   __      _");
+		System.out.println("o'')}____//");
+		System.out.println(" `_/      )\\");
+		System.out.println(" (_(_/-(_/");
+		
+	}
+}
