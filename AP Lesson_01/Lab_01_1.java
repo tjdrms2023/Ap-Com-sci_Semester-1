654b4dbec9e95f47b9217a43ef91fa08661afa26
tjdrms2023 <tjdrms2023@gmail.com>
2016-09-09 10:49:25 -0700

    Lesson_01
    Lab 01

---
+++ AP Lesson_01/Lab_01_1.java
@@ -0,0 +1,10 @@
+public class Lab_01_1
+{
+	public static void main(String[] args)
+	{
+        System.out.println("   __      _");
+		System.out.println("o'')}____//");
+		System.out.println(" `_/      )\\");
+		System.out.println(" (_(_/-(_/");	
+	}
+}
