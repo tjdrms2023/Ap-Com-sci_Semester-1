dbf8f640252bff237be08710b0f51f3ef8647e50
tjdrms2023 <tjdrms2023@gmail.com>
2017-01-27 21:26:59 -0800

    1
    1

--- APLESSON_12/Lab 12.2/Inventory_Runner.java
+++ APLESSON_12/Lab 12.2/Inventory_Runner.java
@@ -3,36 +3,38 @@ public class Inventory_Runner
 {
 	public static void main(String[]args)
 	{
-		Scanner kb=new Scanner(System.in);
-		System.out.println("Will you enter the item category and price information? Y or N: ");
-		String YorN = kb.nextLine();
+		Scanner kb = new Scanner(System.in);
 		
-		if(YorN.equals("Y"))
+		System.out.println("Manufacturer:");
+		String ma = kb.nextLine(); 
+		System.out.println("Name:");
+		String na = kb.nextLine(); 
+		System.out.println("Are you willing to enter category and price information? (y or n)");
+		String YN = kb.nextLine();
+
+		
+		
+		if(YN.equalsIgnoreCase("n"))
 		{
-			System.out.println("Name: ");
-			String name = kb.nextLine();
-			System.out.println("Manufacturer: ");
-			String manufacturer = kb.nextLine();
+			Inventory item1 = new Inventory (ma,na);
+			item1 = new Inventory(ma,na); 
+			System.out.println(item1);	
+			
+		}
+		else if (YN.equalsIgnoreCase("y"))
+		{
+			
 			System.out.println("Category: ");
-			String category = kb.nextLine();
+			String ca = kb.nextLine();
 			System.out.println("Price: ");
-			double price = kb.nextDouble();
-			Inventory item1 = new Inventory(name, manufacturer, category, price);
-			System.out.println(item1);
-		}
-		else
-		{
-			System.out.println("Name: ");
-			String name = kb.nextLine();
-			System.out.println("Manufacturer: ");
-			String manufacturer = kb.nextLine();
-			
-			Inventory item2 = new Inventory(name, manufacturer);
+			double pa = kb.nextDouble();
+			Inventory item2 = new Inventory(ma, na, ca, pa);
 			System.out.println(item2);
-			
 		}
-		
-	}
-	
-	
+		else 
+		{
+			System.out.println("wrong input"); 
+		}
+			
+	}	
 }
